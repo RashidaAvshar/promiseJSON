4e0747bdf3ef959e1a2381add353e2ce66abe1bf
RashidaAvshar <rashidaavshar@gmail.com>
2023-02-08 18:39:20 +0400

    cards

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+// const data = [
+//     {name: "Farid", surname: "Yusif"},
+//     {name: "Fid", surname: "Yif"},
+
+// ];
+
+// document.querySelector("#create-data").addEventListener("click",()=>{
+// localStorage.setItem("user-data", JSON.stringify(data));
+// });
+
+// document.querySelector("#get-data").addEventListener("click", ()=>{
+//     let userData = localStorage.getItem("user-data");
+//     console.log(JSON.parse(userData));
+// })
+
+// document.querySelector("#log-out").addEventListener("click",()=>{
+//     localStorage.removeItem("user-data")
+// })
+
+// !====================================================
+
+document.querySelector("#get-data").addEventListener("click", ()=>{
+    fetch("https://fakestoreapi.com/products")
+            .then((res) =>res.json())
+            .then((json) =>
+            json.map(item=>{
+                document.querySelector(".card-box").innerHTML += `
+                <div class= "card" id="card">
+            <img src=${item.image}/>
+            <h2>${item.title}</h2>
+            <p>${item.description}</p>
+            <p>${item.price} <span>$</span></p>
+        </div>
+             `;
+            }));
+})
